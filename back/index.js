fc1ab6a086491fa7e056d688666248703f65fd6d
UniBreakfast <unibreakfast1@gmail.com>
2023-10-03 14:05:31 +0300

    remove poc

--- back/index.js
+++ back/index.js
@@ -8,13 +8,3 @@ app.use(cors());
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-const data = [
-  { id: 1, name: 'Object 1' },
-  { id: 2, name: 'Object 2' },
-  { id: 3, name: 'Object 3' },
-];
-
-app.get('/api/data', (req, res) => {
-  res.json(data);
-});
